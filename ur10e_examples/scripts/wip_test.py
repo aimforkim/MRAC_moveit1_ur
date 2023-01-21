a965c487a60ea1e49e8c34cebaa5a02ba71e35d4
aimforkim <aimforkim@users.noreply.github.com>
2023-01-21 16:24:04 +0000

    current_poses yaml and test script

---
+++ ur10e_examples/scripts/wip_test.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+from math import pi
+
+import rospy
+from moveit_commander.conversions import list_to_pose
+
+from move_group_utils.move_group_utils import (MoveGroupUtils,
+                                               publish_trajectory_markers,
+                                               poses_list_from_yaml)
+from pilz_robot_program.pilz_robot_program import Lin, Ptp, Sequence
+
+
+def robot_program():
+
+    mgi = MoveGroupUtils()
+    # rospy.sleep(1.0)
+
+    home = (0.0, -pi / 2.0, pi / 2.0, -pi, -pi/2.0, 0.0)
+
+    sequence = Sequence()
+
+    sequence.append(Ptp(goal=home, vel_scale=0.2, acc_scale=0.2))
+
+    # create pose mgs list from yaml
+    poses_list = poses_list_from_yaml(
+        '/dev_ws/src/ur10e_examples/toolpaths/current_poses.yaml')
+
+    poses = [list_to_pose(pose) for pose in poses_list]
+
+    # # publish the poses to rviz for preview
+    # mgi.publish_pose_array(poses)
+
+    # for p in poses:
+    #     sequence.append(Ptp(goal=p, vel_scale=0.2, acc_scale=0.2))
+
+    sequence.append(Ptp(goal=poses[0]))
+    sequence.append(Lin(goal=poses[1]))
+    # sequence.append(Ptp(goal=poses[2], vel_scale=0.2, acc_scale=0.2))
+    # sequence.append(Lin(goal=poses[3], vel_scale=0.2, acc_scale=0.2))
+    sequence.append(Ptp(goal=home, vel_scale=0.2, acc_scale=0.2))
+
+    success, plan = mgi.sequencer.plan(sequence)[:2]
+
+    # mgi.display_trajectory(plan)
+    # publish_trajectory_markers(plan[0])
+
+    if not success:
+        return rospy.logerr(f'{mgi.name}: Failed to plan to sequence')
+    mgi.sequencer.execute()
+
+    return rospy.loginfo('Robot program completed')
+
+
+if __name__ == '__main__':
+
+    robot_program()
